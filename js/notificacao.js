d4724abc856cffc1bdfddf028ae1fbbcc755e742
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2024-12-13 11:17:50 -0300

    aviso de recesso

---
+++ js/notificacao.js
@@ -0,0 +1,6 @@
+const botaoFechar = document.querySelector('.fechar');
+const notificacao = document.querySelector('.container-not');
+
+botaoFechar.addEventListener('click', () => {
+   notificacao.style.display = 'none';
+})
