2aedea08a3e2091f91825fd11f3308742ec70f54
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2025-01-02 11:52:21 -0300

    v1.0.2 (ano novo)

--- js/notificacao.js
+++ js/notificacao.js
@@ -1,6 +1,6 @@
-const botaoFechar = document.querySelector('.fechar');
+/* const botaoFechar = document.querySelector('.fechar');
 const notificacao = document.querySelector('.container-not');
 
 botaoFechar.addEventListener('click', () => {
    notificacao.style.display = 'none';
-})
+}) */
