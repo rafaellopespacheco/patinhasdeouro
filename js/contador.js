657fb04c288348b3d4f43b27a35f9550fe324038
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2025-01-25 13:48:00 -0300

    v.1.0.6
    - Add scroll animation in all pages
    - Changed counter, removed days count

--- js/contador.js
+++ js/contador.js
@@ -9,12 +9,10 @@ function atualizarContador() {
       return;
    }
 
-   const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
    const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
    const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
    const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
 
-   document.getElementById('dias').textContent = dias;
    document.getElementById('horas').textContent = horas;
    document.getElementById('minutos').textContent = minutos;
    document.getElementById('segundos').textContent = segundos;
